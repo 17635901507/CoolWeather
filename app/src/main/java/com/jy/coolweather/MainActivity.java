5992f1c9906aa526db5088f9e5207fb8784aa332
17635901507 <3054286281@qq.com>
2019-09-09 10:42:12 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/jy/coolweather/MainActivity.java
+++ app/src/main/java/com/jy/coolweather/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
